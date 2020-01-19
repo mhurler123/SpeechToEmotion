a044b6ecb7c16a929f056f8d14fe0e32cd807246
Marcel Hurler <mhurler123@googlemail.com>
2020-01-20 00:10:15 +0100

    Integrate different model types and fix some bugs

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import os
 import random
-
 from tqdm import tqdm
 import torch
-
-from modelLSTM import LSTMClassifier
 import dataset
 from torch.utils.data import DataLoader
+from enum import Enum
+
+class ModelType(Enum):
+    LSTM = 0
+    CNN  = 1
 
 # SETTINGS
-DATASET_PATH = os.path.expanduser("./data/")
+MODEL_TYPE = ModelType.CNN
+DATA_DIR = os.path.expanduser("./data/")
 EMB_CACHE = os.path.expanduser("./")
 DATASET_CACHE = os.path.expanduser("./")
 MODEL_CHECKPOINTS = os.path.abspath('./')
 BATCH_SIZE = 20
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('cpu')
 NUM_EPOCHS = 100
 HIDDEN_SIZE = 8 # the LSTM's hidden/output size
 NUM_WORKERS = 8
 LEARNING_RATE = 0.001
+
+if MODEL_TYPE == ModelType.LSTM:
+    from model.lstm import Classifier
+    from model.lstm import collate
+elif MODEL_TYPE == ModelType.CNN:
+    from model.cnn import Classifier
+    from model.cnn import collate
 
 def evaluate(dataloader, net):
     print("Evaluating... ", end="")
@@ -27,7 +38,7 @@ def evaluate(dataloader, net):
     for batchIdx, batch in enumerate(dataloader):
         # extract input and labels
         inputs, labels = batch['features'], batch['label']
-        inputs = inputs.cuda() if torch.cuda.is_available() else inputs.cpu()
+        inputs = inputs.cuda() if DEVICE==torch.device('cuda') else inputs.cpu()
         labels = labels.to(DEVICE)
 
         # predict only
@@ -48,25 +59,34 @@ def evaluate(dataloader, net):
 
 def train(load=False, load_chkpt=None):
     # set up data
-    wholeset, trainset, valset = dataset.loadData(DATASET_PATH+'train.json', 0.2)
+    wholeset, trainset, valset = dataset.loadAndSplitData(DATA_DIR+'train.json',
+                                                          0.2)
     dataloaderTrain = DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True,
-                                 num_workers=0, collate_fn=dataset.collateRNN)
+                                 num_workers=0, collate_fn=collate)
     dataloaderVal   = DataLoader(valset, batch_size=len(valset.indices),
                                  shuffle=True, num_workers=0,
-                                 collate_fn=dataset.collateRNN)
-
-    # set up model and optimizer
+                                 collate_fn=collate)
     featureSize  = wholeset.numFeaturesPerFrame()
     labelSize    = wholeset.labelSize()
-    print(f"Feature size per frame: {featureSize}\nLabel size: {labelSize}")
-    model        = LSTMClassifier(inputSize=featureSize, outputSize=labelSize,
-                                  hiddenSize=HIDDEN_SIZE).to(DEVICE)
+    #print(f"Feature size per frame: {featureSize}\nLabel size: {labelSize}")
+
+    # set up model
+    model = None
+    if MODEL_TYPE == ModelType.LSTM:
+        model = Classifier(inputSize=featureSize, outputSize=labelSize,
+                           hiddenSize=HIDDEN_SIZE).to(DEVICE)
+    elif MODEL_TYPE == ModelType.CNN:
+        model = Classifier().to(DEVICE)
+
+    # set up optimizer
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE,
                                 momentum=0.9)
+
     metric_dict = {'loss': '------', 'accuracy': '------'}
     start_epoch = 0
 
+    # load model from checkpoint
     if load:
         try:
             last_epoch, optimizer_state_dict, loss = model.load(MODEL_CHECKPOINTS, load_chkpt)
@@ -89,7 +109,7 @@ def train(load=False, load_chkpt=None):
         for numBatch, batch in enumerate(dataloaderTrain):
             # extract input and labels
             inputs, labels = batch['features'], batch['label']
-            inputs = inputs.cuda() if torch.cuda.is_available() else inputs.cpu()
+            inputs = inputs.cuda() if DEVICE==torch.device('cuda') else inputs.cpu()
             labels = labels.to(DEVICE)
 
             # zero the parameter gradients
@@ -97,7 +117,7 @@ def train(load=False, load_chkpt=None):
 
             # forward + backward + optimize
             outputs = model(inputs)
-            outputs = outputs.cuda()
+            outputs = outputs.to(DEVICE)
 
             # 2D loss function expects input as (batch, prediction, sequence) and target as (batch, sequence) (containing the class INDEX)
             # loss = criterion(outputs.permute(0,2,1), labels)
@@ -118,6 +138,9 @@ def train(load=False, load_chkpt=None):
         pbar.set_postfix(metric_dict)
         # save model
         model.save(MODEL_CHECKPOINTS, epoch, loss, optimizer)
+
+def predict(filename, load=False, load_chkpt=None, num_samples=1, interactive=False):
+    testData = dataset.LogMelDataset(filename)
 
 def test():
     return True
