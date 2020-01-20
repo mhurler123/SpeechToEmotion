cfd0e4eae798b30815ce17738a2cebd647f7fdda
Marcel Hurler <mhurler123@googlemail.com>
2020-01-20 01:34:51 +0100

    Support reading data without labels & add reverse to onehot

--- dataset.py
+++ dataset.py
@@ -14,8 +14,12 @@ class LogMelDataset(Dataset):
 
     def __getitem__(self, idx):
         raw = self.data[str(idx)]
-        return {'features': torch.FloatTensor(raw['features']),
-                'label': onehot(raw['valence'], raw['activation'])}
+        if 'valence' in raw:
+            return {'features': torch.FloatTensor(raw['features']),
+                    'label': onehot(raw['valence'], raw['activation'])}
+        else:
+            return {'features': torch.FloatTensor(raw['features']),
+                    'label': onehot(0, 0)}
 
     def load(self):
         with open(self.filename) as jsonFile:
@@ -26,7 +30,7 @@ class LogMelDataset(Dataset):
         return len(self.data['0']['features'][0])
 
     def labelSize(self):
-        return 4 # one class for each quarter in the (valence, activation) space
+        return 4 # one class for each quadrant in the (valence, activation) space
 
 def onehot(valence, activation):
     lookup = {(0,0): [1,0,0,0],
@@ -35,11 +39,24 @@ def onehot(valence, activation):
               (1,1): [0,0,0,1]}
     return lookup[(valence, activation)]
 
+def onehotRev(classId):
+    lookup = [(0,0),
+              (1,0),
+              (0,1),
+              (1,1)]
+    return lookup[classId]
+
 def loadAndSplitData(filename, valPerc=0):
     """
     Loads dataset and splits it into training and validation.
+
+        filename   Filename of json dataset
+
+        valPerc    Amount of validation data
+
+        returns    List comprising the whole, training and validation dataset
     """
     data = LogMelDataset(filename)
     splitIndex = int(len(data)*(1-valPerc))
     trainSet, valSet = torch.utils.data.random_split(data,[splitIndex,len(data)-splitIndex])
-    return data, trainSet, valSet
+    return trainSet, valSet, data
